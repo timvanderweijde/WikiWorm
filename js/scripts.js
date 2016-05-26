aef677eaa9eadc5fe5828b08dfe285c7988478ef
timvanderweijde <tim@bluecape.nl>
2016-05-26 21:41:48 +0200

    final

--- js/scripts.js
+++ js/scripts.js
@@ -3,28 +3,29 @@ $(document).ready(function () {
     $('#search-box').focus();
 
     $("#wikiSearch").on("click", function () {
-        getDataFromWikiAPI();
+
+        var text = $("#search-box").val();
+        getDataFromWikiAPI(text);
     });
 
+    $("#wikiRandom").on("click", function () {
+        openRandomWikiPage();
+    });
 });
 
-function getDataFromWikiAPI() {
+function openRandomWikiPage() {
 
-    
-    // Sandbox:
-    // https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&format=json&prop=pageimages%7Cpageterms%7Cinfo&list=&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail&pithumbsize=50&pilimit=10&wbptterms=description&inprop=url&gpssearch=super&gpslimit=20
-    // w/api.php?action=query&format=json&prop=pageimages%7Cpageterms%7Cinfo&list=&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail&pithumbsize=50&pilimit=10&wbptterms=description&inprop=url&gpssearch=super&gpslimit=20
+    window.open('https://en.wikipedia.org/wiki/Special:Random', '_blank');
 
+}
+function getDataFromWikiAPI(searchText) {
 
-    // Sandbox 
-    // https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&format=json&prop=pageimages%7Cextracts&generator=search&formatversion=2&pilimit=20&exsentences=1&exlimit=20&exintro=1&explaintext=1&gsrsearch=superman&gsrnamespace=0&gsrlimit=10
-    //  /w/api.php?action=query&format=json&prop=pageimages%7Cextracts&generator=search&formatversion=2&pilimit=20&exsentences=1&exlimit=20&exintro=1&explaintext=1&gsrsearch=superman&gsrnamespace=0&gsrlimit=10
-
+    // final 
     // Sandbox
-    // https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&format=json&prop=pageimages%7Cextracts%7Cinfo&generator=search&formatversion=2&pilimit=20&exsentences=1&exlimit=20&exintro=1&explaintext=1&inprop=url&gsrsearch=superman&gsrnamespace=0&gsrlimit=10
-    // w/api.php?action=query&format=json&prop=pageimages%7Cextracts%7Cinfo&generator=search&formatversion=2&pilimit=20&exsentences=1&exlimit=20&exintro=1&explaintext=1&inprop=url&gsrsearch=superman&gsrnamespace=0&gsrlimit=10
+    // https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&format=json&prop=extracts%7Cinfo&generator=search&formatversion=2&exsentences=1&exlimit=20&exintro=1&explaintext=1&inprop=url&gsrsearch=superman&gsrnamespace=0&gsrlimit=10
+    // /w/api.php?action=query&format=json&prop=extracts%7Cinfo&generator=search&formatversion=2&exsentences=1&exlimit=20&exintro=1&explaintext=1&inprop=url&gsrsearch={{searchvalue}}&gsrnamespace=0&gsrlimit=10
 
-    var playListURL = 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages%7Cextracts%7Cinfo&generator=search&formatversion=2&pilimit=20&exsentences=1&exlimit=20&exintro=1&explaintext=1&inprop=url&gsrsearch=superman&gsrnamespace=0&gsrlimit=10&callback=?';
+    var playListURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cinfo&generator=search&formatversion=2&exsentences=1&exlimit=20&exintro=1&explaintext=1&inprop=url&gsrsearch={{searchvalue}}&gsrnamespace=0&gsrlimit=10&callback=?";
 
     function parseData(data) {
 
@@ -32,43 +33,27 @@ function getDataFromWikiAPI() {
         var page_value = "";
 
         var html = "<tbody>";
-        
+
         $.each(data.query.pages, function (i, item) {
 
             html += "<tr><td>";
-            
-            html += "<h4><a target=\"_blank\" href=\"" + item.fullurl +"\">" + item.title + "</a></h4>";
-            //console.log(item.pageid + ' ' + item.title + ' ');
 
-            if (item.terms) {
-                //console.log(item.terms.description[0]);
-                 
-            }
-
-            if (item.thumbnail) {
-               // console.log(item.thumbnail.source + ', ' + item.thumbnail.width + 'px, ' + item.thumbnail.height + 'px');
-            }
+            html += "<h4><a target=\"_blank\" href=\"" + item.fullurl + "\">" + item.title + "</a></h4>";
 
             if (item.extract) {
-               // console.log(item.extract)
-               html += "<p>" + item.extract + "</p>";
+                html += "<p>" + item.extract + "</p>";
             }
-            
-             html += "</td></tr>";
+
+            html += "</td></tr>";
         });
-        
+
         html += "</tbody>";
-        
-        //console.log(html)
-        
+
         $(".table.result").html(html);
-        // $("#quote").animateCss('zoomIn');
 
-        // $("#quote").empty();
-        // $("#quote").append(data.value.joke);
-
-        // changeTweetLink(data.value.joke);
+        $(".table.result").removeClass("hidden");
     }
 
-    $.getJSON(playListURL, parseData);
+    var url = playListURL.replace("{{searchvalue}}", encodeURIComponent(searchText));
+    $.getJSON(url, parseData);
 }
